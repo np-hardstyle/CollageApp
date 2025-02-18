066877a6a33a7778295189cb3658397df413c046
John <60302546+np-hardstyle@users.noreply.github.com>
2025-02-17 21:21:40 -0800

    Font changes

--- CollageApp/MainWindow.xaml.cs
+++ CollageApp/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Specialized;
 using System.Data.SqlClient;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
@@ -19,24 +20,50 @@ namespace CollageApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool Enable_Properties = false;
         public MainWindow()
         {
             InitializeComponent();
 
-            // setup image stack for user inputted images (empty by default)
-            
+            // filetab font
+            FileTab.FontWeight = FontWeights.Medium;
+
             // setup grid toggle button
             GridCheckBox.IsChecked = true;
             GridCheckBox.Checked += CheckBox_Checked;
             GridCheckBox.Unchecked += CheckBox_Unchecked;
+            GridCheckBox.FontWeight = FontWeights.Medium;
 
             // setup file open button
             FileOpenButton.IsEnabled = true;
             FileOpenButton.Click += MenuItem_Click;
+            FileOpenButton.FontWeight = FontWeights.Medium;
 
             // properties
             Properties.Click += Properties_Click;
+            Properties.Background = null;
+            Properties.BorderBrush = null;
+            Properties.FontWeight = FontWeights.Medium;
+
+            // window size change event
+            //SizeChanged += MainWindow_SizeChanged;
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ChangeFontSize(this, e.NewSize.Width * 0.015);
+        }
+
+        private void ChangeFontSize(DependencyObject parent, double newFontSize)
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+                if (child is Control control)
+                {
+                    control.FontSize = newFontSize;
+                }
+                ChangeFontSize(child, newFontSize);
+            }
         }
 
         private void Properties_Click(object sender, RoutedEventArgs e)
